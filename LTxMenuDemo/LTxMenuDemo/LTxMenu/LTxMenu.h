0024d7168076ad7807a7f73d2ba582cfce1940e2
liangtong <l900416@163.com>
2017-05-19 00:02:19 +0800

    update

--- LTxMenuDemo/LTxMenuDemo/LTxMenu/LTxMenu.h
+++ LTxMenuDemo/LTxMenuDemo/LTxMenu/LTxMenu.h
@@ -9,6 +9,37 @@
 #import <UIKit/UIKit.h>
 #import "LTxMenuItem.h"
 
+/**
+ * version : 2.0
+ * 变更内容：使用delegate和datasource
+ *
+ **/
+
+
+#pragma mark LTxMenuDelegate
+@protocol LTxMenuDelegate<NSObject>
+
+@optional
+-(void)didSelectRowAtIndex:(NSInteger)index;
+-(void)didSelectAccessoryView:(UIView*)accessoryView
+                      atIndex:(NSInteger)index;
+@end
+
+#pragma mark LTxMenuDataSource
+@protocol LTxMenuDataSource<NSObject>
+
+@required
+- (NSInteger)numberOfRows;
+
+@optional
+- (NSString*)titleForRowAtIndex:(NSInteger)index;
+- (NSAttributedString*)attributedTitleForRowAtIndex:(NSInteger)index;
+- (UIImage*)imageForRowAtIndex:(NSInteger)index;
+- (NSArray<UIView*>*)accessoryViewsAtIndex:(NSInteger)index;
+@end;
+
+
+
 @interface LTxMenuView : UIView
 
 @property (nonatomic,copy) int(^numberOfRows)();
